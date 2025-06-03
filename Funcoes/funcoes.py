9dd2e5acbf0dafb2284ac2dd1535a0a6a2a1201f
bevenutoportuga@gmail.com <bevenutoportuga@gmail.com>
2025-06-03 19:30:06 -0300

    Exercicios de Funcoes

---
+++ Funcoes/funcoes.py
@@ -0,0 +1,37 @@
+notas = [7.5,8.0,6.5,9.0]
+media = sum(notas) / len(notas) # len significa quantidade
+
+print(f"Media das notas: {media:.2f}")
+
+numeros = [145,275,450,500]
+
+soma = sum(numeros)
+print(soma)
+print(f"A soma dos numeros: {soma}")
+
+valores = [7.5,8.0,6.5,9.0]
+maior = max(valores)
+
+numeros = [7.5,8.0,6.5,9.0]
+menor = min(numeros)
+
+print(f"Maior Valor: {maior}")
+print(f"Menor Valor: {menor}")
+
+dados = [7.5,8.0,6.5,9.0]
+quantidade = len(dados)
+
+print(f"Quantidade de notas cadastradas: {quantidade}")
+
+qtde = [7.5,8.0,6.5,9.0]
+qtde.sort() #organi
+
+print(f"Notas em ordem crescente: {qtde}")
+print(qtde)
+
+qtde = [7.5,8.0,6.5,9.0]
+qtde.sort(reverse=True) #organiza a lista em ordem decresente
+
+print(f"Notas em ordem decrescente: {qtde}")
+
+
