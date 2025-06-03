775261fc46ec993ace4661d2e94792a4e48ff333
bevenutoportuga@gmail.com <bevenutoportuga@gmail.com>
2025-06-03 19:13:32 -0300

    Aulas de If, Elif e Else

---
+++ If_elif_Else/condicionais.py
@@ -0,0 +1,42 @@
+#condicionais
+numero = -5
+
+if numero >= 0:
+    print("O numero é positivo")
+else:
+    print ("O numero é negativo")
+
+    valor = 7
+
+if valor % 2 == 0:
+        print ("O numero é par")
+
+else:
+        print ("o numero é impar")
+
+        nota = 7
+if nota >= 7:
+         print ("Aprovado")
+
+elif nota >= 5:
+    print("Recuperacao")
+
+else:
+    print ("Reprovado")
+
+
+idade = int(input("Digite a idade do jovem ")) 
+
+if idade < 18:
+    print ("Nao convocado")
+
+elif idade <= 21:  
+    print ("convocado")  
+
+else:
+    print ("Dispensado")
+
+
+
+
+
