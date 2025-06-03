b145f1f1e8ceb43dc720261c1f5ab69347cd80b6
bevenutoportuga@gmail.com <bevenutoportuga@gmail.com>
2025-06-03 19:34:26 -0300

    Exercicios para pegar posicao

---
+++ Pegar_Posicao/pegarposicao.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+time.sleep(5)
+pyautogui.click()
+
+print(pyautogui.position())
+
+# # #print(pyautogui.size())
+# pyautogui.press('win')
+# time.sleep(3)573802Chpw
+
+# pyautogui.write('573802Chpw')
